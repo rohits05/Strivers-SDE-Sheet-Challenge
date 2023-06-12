1a4bf12a150f2fe1b53c0b4930751b1960e1a535
Rohit Shaw <carpediem212045@gmail.com>
2023-06-12 09:09:08 +0530

    Update and rename Majority Element.java to Unique Paths.java

---
+++ Arrays Part - III/Unique Paths.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+    public static int uniquePaths(int m, int n) {
+        // Write your code here.
+        int dp[][] = new int[m][n]; // Tabulation ! TC: O(MxN) | SC: O(MxN)
+        dp[0][0] = 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0)
+                    dp[i][j] = 1; // Base case
+                else {
+                    int up = 0, left = 0; // bottom - up movement !
+                    // checks for valid moves
+                    if (i > 0)
+                        up = dp[i - 1][j];
+                    if (j > 0)
+                        left = dp[i][j - 1];
+
+                    dp[i][j] = up + left;
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
