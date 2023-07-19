a632f3fa2af027603218bcb98825034c36096fd9
Rohit Shaw <carpediem212045@gmail.com>
2023-07-19 10:43:58 +0530

    Add files via upload

--- Binary Search/Single Element in a Sorted Array.java
+++ Binary Search/Single Element in a Sorted Array.java
@@ -1,19 +1,31 @@
 import java.util.ArrayList;
+import java.util.*;
+import java.io.*;
 
 public class Solution {
-    public static int singleNonDuplicate(ArrayList<Integer> nums) {
+    public static int singleNonDuplicate(ArrayList<Integer> arr) {
         // Write your code here.
-        int start = 0, end = nums.size() - 1;
+        int len = arr.size(), low = 0, high = len - 1;
 
-        while (start < end) {
-            int mid = (start + end) / 2;
-            if ((mid % 2 == 0 && nums.get(mid) == nums.get(mid + 1)) ||
-                    (mid % 2 == 1 && nums.get(mid) == nums.get(mid - 1)))
-                start = mid + 1;
-            else
-                end = mid;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+            if (mid % 2 == 1) {
+                int firstmid = arr.get(mid), lastmid = arr.get(mid - 1);
+
+                if (firstmid == lastmid)
+                    low = mid + 1;
+                else
+                    high = mid;
+            } else if (mid % 2 == 0) {
+                int nextmid = arr.get(mid), nextLmid = arr.get(mid + 1);
+
+                if (nextmid == nextLmid)
+                    low = mid + 1;
+                else
+                    high = mid;
+            }
         }
 
-        return nums.get(start);
+        return arr.get(low);
     }
 }
