8602906668889c7f597366d65cd31b2b5789e0c4
Rohit Shaw <carpediem212045@gmail.com>
2023-07-19 10:50:57 +0530

    Add files via upload

--- Linked List/Add Two Numbers.java
+++ Linked List/Add Two Numbers.java
@@ -1,45 +1,33 @@
-import java.util.*;
-import java.io.*;
-
-/****************************************************************
- * 
- * Following is the class structure of the Node class:
- * 
- * class LinkedListNode {
- * int data;
- * LinkedListNode next;
- * 
- * public LinkedListNode(int data) {
- * this.data = data;
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
- * }
- * 
- *****************************************************************/
-
-public class Solution {
-    static LinkedListNode addTwoNumbers(LinkedListNode head1, LinkedListNode head2) {
-        // Write your code here.
-        LinkedListNode dummyHead = new LinkedListNode(0);
-        LinkedListNode head = dummyHead;
+ */
+class Solution { // Always getting an unknown error at the Coding Ninjas site for the Java solution only. So, I've performed it on Leetcode.
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode head = dummyHead;
         int carry = 0;
-
-        while (head1 != null || head2 != null) {
-            int val1 = head1 == null ? 0 : head1.data; // tnc
-            int val2 = head2 == null ? 0 : head2.data;
-
+        
+        while(l1 != null || l2 != null) {
+            int val1 = l1 == null ? 0 : l1.val; // tnc
+            int val2 = l2 == null ? 0 : l2.val;
+            
             int sum = val1 + val2 + carry;
-            head.next = new LinkedListNode(sum % 10); // opt
+            head.next = new ListNode(sum % 10); // opt
             carry = sum / 10;
             head = head.next;
-
-            if (head1 != null)
-                head1 = head1.next;
-            if (head2 != null)
-                head2 = head2.next;
+            
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
         }
-
-        if (carry > 0)
-            head.next = new LinkedListNode(carry); // fwd carry
+        
+        if(carry > 0) head.next = new ListNode(carry); // fwd carry
         return dummyHead.next;
     }
 }
